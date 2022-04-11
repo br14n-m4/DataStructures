fed2b0ab8cc0aaa96f71ef258f37886930a55126
br14n-m4 <br14n.m4@gmail.com>
2022-04-11 19:06:00 -0400

    Some Bugs Fixed

--- Graph/Bellman-Ford.cpp
+++ Graph/Bellman-Ford.cpp
@@ -9,7 +9,7 @@ vector<tuple<int, int, int>> edge_c;
 
 vector<int> bellman_ford(int u, vector<int> distance){
     distance[u] = 0;
-    for (int i = 0; i < N - 1; ++i){
+    for (int i = 0; i < distance.size() - 1; ++i){
         for (auto &x : edge_c){
             int v, j, cost;
             tie(v, j, cost) = x;
